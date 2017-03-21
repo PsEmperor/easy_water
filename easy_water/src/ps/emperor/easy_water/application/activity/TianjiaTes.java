d51afe44789a97f48fe980c1f9bd8e1350da85db
PS <psemperor@yeah.net>
2017-03-21 10:50:45 +0800

    添加个测试提交类。。。。。。。。。。。。。。。。2017.3.21

---
+++ easy_water/src/ps/emperor/easy_water/application/activity/TianjiaTes.java
@@ -0,0 +1,15 @@
+package ps.emperor.easy_water.application.activity;
+
+import android.os.Bundle;
+import ps.emperor.easy_water.BaseActivity;
+
+public class TianjiaTes extends BaseActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		System.out.println(getClass().getSimpleName());
+	}
+	
+	
+}
